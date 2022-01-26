a28b00264fde394065fe6037010c87c988d70135
ilker <ilkerciliv@gmail.com>
2022-01-26 00:42:30 -0500

    pick random rolls from array

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,24 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    // IBOutlet allows me to referenfce a UI element
+    
     @IBOutlet weak var diceImageViewOne: UIImageView!
     @IBOutlet weak var diceImageViewTwo: UIImageView!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // diceImageViewOne.image = #imageLiteral(resourceName: "DiceSix")
-        diceImageViewOne.image = #imageLiteral(resourceName: "DiceSix")
-        diceImageViewTwo.image = #imageLiteral(resourceName: "DiceTwo")
-    }
+    // let is for variables that are constant, var is for variables
+    let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
     
-    // Connection : Action, Type : UI button, Event : Touch Ip Inside
+        
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        print("Button got tapped.")
-        diceImageViewOne.image = #imageLiteral(resourceName: "DiceFour")
-        diceImageViewTwo.image = #imageLiteral(resourceName: "DiceFour")
+        
+        diceImageViewOne.image = diceArray[Int.random(in: 0...5)]
+        diceImageViewTwo.image = diceArray[Int.random(in: 0...5)]
+
+        
     }
     
 }
